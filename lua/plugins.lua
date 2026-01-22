8244cc871ca5950fcd7bc9d4c918f7f4e928551d
tprost <tprost@users.noreply.github.com>
2026-01-21 19:50:59 -0500

    theming and config stuff

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,3 @@
-	
-
 -- ~/.config/nvim-new/lua/plugins.lua
 vim.pack.add({
     { src = "https://github.com/lewis6991/gitsigns.nvim" },
@@ -31,4 +29,60 @@ vim.pack.add({
     { src = "https://github.com/NeogitOrg/neogit" },
 })
 
+-- Add Monokai Pro theme
+vim.pack.add({
+    { src = "https://github.com/loctvl842/monokai-pro.nvim" },
+})
 
+
+require("monokai-pro").setup({
+  transparent_background = true,
+  terminal_colors = true,
+  devicons = true,
+  styles = {
+    comment = { italic = true },
+    keyword = { italic = true },
+    type = { italic = true },
+    storageclass = { italic = true },
+    structure = { italic = true },
+    parameter = { italic = true },
+    annotation = { italic = true },
+    tag_attribute = { italic = true },
+  },
+  filter = "octagon",
+  day_night = {
+    enable = false,
+    day_filter = "pro",
+    night_filter = "spectrum",
+  },
+  inc_search = "background", -- underline | background
+  background_clear = {
+    "toggleterm",
+    "telescope",
+    "renamer",
+    "notify",
+  },
+  plugins = {
+    bufferline = {
+      underline_selected = false,
+      underline_visible = false,
+      underline_fill = false,
+      bold = true,
+    },
+    indent_blankline = {
+      context_highlight = "default", -- default | pro
+      context_start_underline = false,
+    },
+  },
+  override = function(scheme)
+    return {}
+  end,
+  override_palette = function(filter)
+    return {}
+  end,
+  override_scheme = function(scheme, palette, colors)
+    return {}
+  end,
+})
+
+vim.cmd.colorscheme("monokai-pro")
